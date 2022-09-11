b8bb3083f7bc490ce37cf4640472b66567c41e6d
crmh <t260robo@gmail.com>
2022-09-11 16:12:11 +0900

    top画面データ取得・作成

--- app/pages/api/graphql.ts
+++ app/pages/api/graphql.ts
@@ -8,11 +8,36 @@ const typeDefs = gql`
   type Type {
     id: ID!
     type_name: String!
+    management: [ManagementData]
+  }
+
+  type ManagementData {
+    id: ID!
+    type_id: ID!
+    data_name: String!
+    current_num: Int!
+    data_history: [DataHistory]
+  }
+
+  type DataHistory {
+    id: ID!
+    management_id: ID!
+    change_num: Int!
+    change_reason: String!
+    comment: String
+    change_date: String!
   }
 
   type Query {
-    hello: String
     types: [Type]
+  }
+
+  type Mutation {
+    createType(type_name: String!): Type
+  }
+
+  input TypeInput {
+    type_name: String!
   }
 `;
 
@@ -22,11 +47,29 @@ interface Context {
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello World',
     types: async (parent: undefined, args: {}, context: Context) => {
-      return await context.prisma.type_mst.findMany();
+      return await context.prisma.type_mst.findMany({
+        where: { delete_flg: false },
+        select: {
+          id: true,
+          type_name: true,
+          management: {
+            where: { delete_flg: false }
+          }
+        }
+      });
     }
   },
+  Mutation: {
+    createType: async (parent: undefined, args: {type_name: string}, context: Context) => {
+      return await context.prisma.type_mst.create({
+        data: {
+          type_name: args.type_name,
+          delete_flg: false,
+        }
+      })
+    }
+  }
 };
 
 const apolloServer = new ApolloServer({
