f4fec971a8b9d8e6774ead6348e1648294a36a1c
crmh <t260robo@gmail.com>
2022-09-15 01:14:26 +0900

    管理データ画面作成

--- app/pages/api/graphql.ts
+++ app/pages/api/graphql.ts
@@ -1,43 +1,66 @@
 import { ApolloServer, gql } from "apollo-server-micro";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { PrismaClient } from "@prisma/client";
+import { management_data, PrismaClient } from "@prisma/client";
+import { GraphQLScalarType, Kind } from "graphql";
+
+const dateScalar = new GraphQLScalarType({
+  name: 'Date',
+  description: 'Date custom scalar type',
+  serialize(value: any) {
+    return value.getTime(); // Convert outgoing Date to integer for JSON
+  },
+  parseValue(value: any) {
+    return new Date(value); // Convert incoming integer to Date
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+    }
+    return null; // Invalid hard-coded value (not an integer)
+  },
+});
 
 const prisma = new PrismaClient();
 
 const typeDefs = gql`
+  scalar Date
+ 
   type Type {
-    id: ID!
+    id: Int!
     type_name: String!
     management: [ManagementData]
   }
 
   type ManagementData {
-    id: ID!
-    type_id: ID!
+    id: Int!
+    type_id: Int!
     data_name: String!
     current_num: Int!
     data_history: [DataHistory]
   }
 
   type DataHistory {
-    id: ID!
-    management_id: ID!
+    id: Int!
+    management_id: Int!
     change_num: Int!
     change_reason: String!
     comment: String
-    change_date: String!
+    change_date: Date
   }
 
   type Query {
     types: [Type]
+    managementData(type_id: Int!): [ManagementData]
   }
 
   type Mutation {
     createType(type_name: String!): Type
+    createData(management_data: DataInput!): ManagementData
   }
 
-  input TypeInput {
-    type_name: String!
+  input DataInput {
+    type_id: Int!
+    data_name: String!
   }
 `;
 
@@ -46,6 +69,7 @@ interface Context {
 }
 
 const resolvers = {
+  Date: dateScalar,
   Query: {
     types: async (parent: undefined, args: {}, context: Context) => {
       return await context.prisma.type_mst.findMany({
@@ -58,6 +82,23 @@ const resolvers = {
           }
         }
       });
+    },
+    managementData: async (parent: undefined, args: {type_id: number}, context: Context) => {
+      return await context.prisma.management_data.findMany({
+        where: {
+          AND: [
+            {
+              type_id: args.type_id,
+            },
+            {
+              delete_flg: false
+            }
+          ]
+        },
+        include: {
+          data_history: true,
+        }
+      })
     }
   },
   Mutation: {
@@ -66,6 +107,16 @@ const resolvers = {
         data: {
           type_name: args.type_name,
           delete_flg: false,
+        }
+      })
+    },
+    createData: async (parent: undefined, args: {management_data: management_data}, context: Context) => {
+      return await context.prisma.management_data.create({
+        data: {
+          type_id: args.management_data.type_id,
+          data_name: args.management_data.data_name,
+          delete_flg: false,
+          current_num: 0,
         }
       })
     }
